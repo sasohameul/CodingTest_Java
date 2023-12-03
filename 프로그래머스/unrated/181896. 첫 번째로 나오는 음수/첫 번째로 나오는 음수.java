2c7df5740101e118338f5899a34b51da5da849d3
sasohameul <133090074+sasohameul@users.noreply.github.com>
2023-12-03 13:58:46 +0900

    [level unrated] Title: 첫 번째로 나오는 음수, Time: 0.02 ms, Memory: 76 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        for(int n : num_list) {
+	        	
+	        	if(n < 0) {
+	        		
+	        		return answer;
+	        	} 
+	        	
+	        	answer++;
+	        }
+	        
+	        
+        return -1;
+    }
+}
