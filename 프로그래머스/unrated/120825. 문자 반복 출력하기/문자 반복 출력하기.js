ea777f4f2c7a74508fe8f4f197fa332df5ce6229
sasohameul <133090074+sasohameul@users.noreply.github.com>
2023-12-15 10:26:46 +0900

    [level unrated] Title: 문자 반복 출력하기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120825. 문자 반복 출력하기/문자 반복 출력하기.js
@@ -0,0 +1,13 @@
+function solution(my_string, n) {
+    var answer = '';
+     for(var i = 0; i < my_string.length; i++){
+
+      for(var j = 0; j < n; j++){
+
+        answer += my_string[i];
+
+      }
+
+    }
+    return answer;
+}
