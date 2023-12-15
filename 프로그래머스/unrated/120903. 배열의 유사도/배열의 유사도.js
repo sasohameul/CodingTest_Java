58a19085390273a9355c77ab4eef896dc6662678
sasohameul <133090074+sasohameul@users.noreply.github.com>
2023-12-16 08:53:50 +0900

    [level unrated] Title: 배열의 유사도, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120903. 배열의 유사도/배열의 유사도.js
+++ 프로그래머스/unrated/120903. 배열의 유사도/배열의 유사도.js
@@ -1,14 +1,5 @@
 function solution(s1, s2) {
-     var answer = 0;
-
-  for(var i = 0; i < s1.length; i++){
-
-    for(var j = 0; j < s2.length; j++){
-
-      if(s1[i] === s2[j]) answer++;
-
-    }
-
-  }
-  return answer;
+     const result = s1.filter( r => s2.includes(r));
+  
+  return result.length;
 }
