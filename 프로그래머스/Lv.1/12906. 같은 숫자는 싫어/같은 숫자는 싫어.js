fcb7a9ca0b34d68e238313efe8cf51c7c19dd4d6
yoonjeong <133090074+sasohameul@users.noreply.github.com>
2024-01-13 23:28:02 +0900

    [level Lv.1] Title: 같은 숫자는 싫어, Time: 22.29 ms, Memory: 83.3 MB -BaekjoonHub

--- 프로그래머스/Lv.1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/Lv.1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,12 +1,13 @@
+  
+  
 
 const solution = (arr) => {
 
   const result = [];
   
   arr.forEach((num, idx) => {
-    if (idx === 0 || arr[idx - 1] !== num) {
-      result.push(num);
-    }
+      
+    if (idx === 0 || arr[idx - 1] !== num) result.push(num);
   });
 
   return result;
